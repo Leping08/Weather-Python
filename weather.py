d9601b0084cc32fab90123831661b1c9ed4d067c
Derek Schmitt <Derek@Dereks-MacBook-Pro.local>
2019-04-23 20:55:14 -0400

    Add file that will read temp humidity and pressure

---
+++ weather.py
@@ -0,0 +1,52 @@
+import time
+import dht11
+import datetime
+import requests
+import RPi.GPIO as GPIO
+try:
+    from smbus2 import SMBus
+except ImportError:
+    from smbus import SMBus
+from bmp280 import BMP280
+
+
+
+print('/////////////////////////////') 
+print('//Derek\'s Weather Station///') 
+print('/////////////////////////////')  
+print('/////////////   /////////////') 
+print('////////////   //////////////') 
+print('///////////   ///////////////') 
+print('//////////      /////////////') 
+print('////////////   //////////////') 
+print('///////////   ///////////////') 
+print('//////////   ////////////////') 
+print('///////////   ///////////////') 
+print('//////////  /////////////////') 
+print('///////// ///////////////////') 
+print('/////////////////////////////')
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+
+
+bus = SMBus(1)
+bmp280 = BMP280(i2c_dev=bus)
+
+
+instance = dht11.DHT11(pin=14)
+
+while True: 
+    result = instance.read() 
+    if result.is_valid(): 
+        temperature = bmp280.get_temperature()
+        pressure = bmp280.get_pressure()
+        print("Humidity %d %%" % result.humidity)
+        print("Pressure {:05.2f}hPa" % pressure)
+        print("Temp {:05.2f}*F" % ((temperature * 9/5) + 35))
+        time.sleep(1)
+
+
+GPIO.cleanup()
