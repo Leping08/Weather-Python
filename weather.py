0c1d25c90b72bccead5be57a590096317d24e4e3
Derek Schmitt <Derek@Dereks-MacBook-Pro.local>
2019-05-01 21:35:22 -0400

    Null out the first seasor reading

--- weather.py
+++ weather.py
@@ -40,21 +40,28 @@ bmp280 = BMP280(i2c_dev=bus)
 instance = dht11.DHT11(pin=14)
 
 url = 'http://192.168.1.14:8000/api/weather/eMGzj8KOyITdwgnJA1Gd'
-#url = 'http://192.168.1.14:8000/api/wee'
+
+count = 0
 
 while True: 
     result = instance.read() 
     if result.is_valid(): 
-        temperature = bmp280.get_temperature()
-        pressure = bmp280.get_pressure()
-        temp_fahrenheit = ((temperature * 9/5) + 35)
-        now = datetime.datetime.now()
-        print("Humidity %d %%" % result.humidity)
-        print("Pressure {:05.2f}hPa".format(pressure))
-        print("Temp {:05.2f}*F".format(temp_fahrenheit))
-        print(now.strftime("%Y-%m-%d %H:%M:%S"))
-        payload = {"temp": temp_fahrenheit, "humidity": result.humidity, "pressure": pressure, "event_time": str(now)}
+        if count >= 1: # Only read the saesor data after the first reading
+            temperature = bmp280.get_temperature()
+            pressure = bmp280.get_pressure()
+            temp_fahrenheit = ((temperature * 9/5) + 35)
+            now = datetime.datetime.now()
+            print("Humidity %d %%" % result.humidity)
+            print("Pressure {:05.2f}hPa".format(pressure))
+            print("Temp {:05.2f}*F".format(temp_fahrenheit))
+            print(now.strftime("%Y-%m-%d %H:%M:%S"))
+            payload = {"temp": temp_fahrenheit, "humidity": result.humidity, "pressure": pressure, "event_time": str(now)}
+        else: # Read the data for the first time and null it out. Most of the sensor data on the first pass is bad data
+            temperature = bmp280.get_temperature()
+            pressure = bmp280.get_pressure()
+            payload = {"temp": "null", "humidity": "null", "pressure": "null", "event_time": str(now)}
         r = requests.post(url, json=payload)
+        count = count + 1
         time.sleep(1)
 
 
